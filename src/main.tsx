38dd78088dbcb21abbefc5b92cd4fc3be20356f5
Viniciussilver <viniciussilverk@gmail.com>
2023-01-10 19:02:46 -0300

    refactor: organizado importação

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
 import { App } from './routes/routes';
+import { MenuLateral } from './shared/components';
 import { DrawerProvider } from './shared/contexts/DrawerContext';
 import { AppThemeProvider } from './shared/contexts/ThemeContext';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <AppThemeProvider>
-      <DrawerProvider>
-        <App />
-      </DrawerProvider>
-    </AppThemeProvider>
+    <BrowserRouter>  
+      <AppThemeProvider>
+        <DrawerProvider>
+          <MenuLateral>
+            <App />
+          </MenuLateral>
+        </DrawerProvider>
+      </AppThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
